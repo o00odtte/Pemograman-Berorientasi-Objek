0fc99a31b459bd7a002dd166efd3751a06acb1df
o00odtte <onamuuue@gmail.com>
2026-02-05 14:51:41 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,7 @@
+package Pertemuan1;
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Paman Berorientasi Objek");
+        System.out.println("Ini adalah program Java pertama saya");
+    }
+}
---
+++ Pertemuan1/KodePertama_Latihan.java
@@ -0,0 +1,17 @@
+package Pertemuan1;
+
+public class KodePertama_Latihan {
+    public static void main(String[] args){
+        System.out.println("Nama : Oktavia Namu Erdita");
+        System.out.println("NIM : D1041241072");
+        System.out.println("Semester : 4");
+        System.out.println("Prodi : Informatika");
+    }
+}
+
+/* Nama : Oktavia Namu Erdita 
+NIM : D1041241072
+Tangal : Kamis, 5 Februari 2026
+*/
+
+
