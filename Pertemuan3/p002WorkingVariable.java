dcee9014f5d26caf15a16f86c8c7401fcfcb464f
o00odtte <onamuuue@gmail.com>
2026-02-12 14:36:00 +0700

    Pertemuan3

---
+++ Pertemuan3/p002WorkingVariable.java
@@ -0,0 +1,18 @@
+package Pertemuan3;
+
+public class p002WorkingVariable {
+    public static void main(String[] args) {
+        //Deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        //Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: "+ angka);
+    }
+    
+}
---
+++ Pertemuan3/p023PrimitiveData.java
@@ -0,0 +1,33 @@
+package Pertemuan3;
+
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        //Tipe data bilangan buat
+        int umur = 20;
+        long populasi = 8000000000L;
+
+        //Tipe data bilangan desimal
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+
+        //Tipe data karakter 
+        char nilaiHuruf = 'A';
+
+        //Tipe data logika
+        boolean lulus = true;
+
+        //Menampilkan nilai semua variabel
+        System.out.println("Umur            : " + umur);
+        System.out.println("Populasi        : " + populasi);
+        System.out.println("Tinggi Badan    : " + tinggi);
+        System.out.println("IPK             : " + ipk);
+        System.out.println("Nilai Huruf     : " + nilaiHuruf);
+        System.out.println("Status Lulus    : " + lulus);
+
+    }
+}
+
+/* Nama : Oktavia Namu Erdita 
+NIM : D1041241072
+Tangal : Kamis, 12 Februari 2026
+*/
