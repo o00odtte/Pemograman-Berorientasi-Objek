e161d2ea589741cb3228064127b7093968f77f1b
o00odtte <onamuuue@gmail.com>
2026-02-12 14:08:30 +0700

    Pertemuan3

---
+++ Pertemuan3/p002WorkingVariable.java
@@ -0,0 +1,23 @@
+package Pertemuan3;
+
+public class p002WorkingVariable {
+    public static void main(String[] args) {
+        //Deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        //Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: "+ angka);
+    }
+    
+}
+
+/* Nama : Oktavia Namu Erdita 
+NIM : D1041241072
+Tangal : Kamis, 12 Februari 2026
+*/
