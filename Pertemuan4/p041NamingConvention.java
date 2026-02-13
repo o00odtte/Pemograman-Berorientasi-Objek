99d83c7f75f2c55450e6b5629ba9f18d6774f50c
o00odtte <onamuuue@gmail.com>
2026-02-13 08:34:35 +0700

    Pertemuan4

---
+++ Pertemuan4/p041NamingConvention.java
@@ -0,0 +1,21 @@
+package Pertemuan4;
+
+public class p041NamingConvention {
+    public static final double PI = 3.14;
+public static void main(String[] args) {
+// Variabel dengan camelCase
+int studentAge = 20;
+String studentName = "Budi";
+boolean isActive = true;
+System.out.println("Nama        : " + studentName);
+System.out.println("Umur        : " + studentAge);
+System.out.println("Status      : " + isActive);
+System.out.println("Konstanta PI: " + PI);
+}
+}
+
+/*
+Nama : Oktavia Namu Erdita
+NIM : D1041241072
+Jumat, 13 Februari 2026
+*/
---
+++ Pertemuan4/p042OperatorAritmatika.java
@@ -0,0 +1,25 @@
+package Pertemuan4;
+
+public class p042OperatorAritmatika {
+    public static void main(String[] args) {
+int a = 10;
+int b = 3;
+System.out.println("a = " + a);
+System.out.println("b = " + b);
+System.out.println();
+System.out.println("a + b = " + (a + b));
+System.out.println("a - b = " + (a - b));
+System.out.println("a * b = " + (a * b));
+System.out.println("a / b = " + (a / b));
+System.out.println("a % b = " + (a % b));
+// Pembagian dengan desimal
+double hasil = (double) a / b;
+System.out.println("a / b (desimal) = " + hasil);
+    }
+}
+
+/*
+Nama : Oktavia Namu Erdita
+NIM : D1041241072
+Jumat, 13 Februari 2026
+*/
